ae70e2af555f7768a0f94818f857216d677b2c2d
留岡佑太郎 <tomeokayutaro@RLSUU103293M01.local>
2017-04-18 19:21:27 +0900

    カルーセルプッシュ！

--- js/common.js
+++ js/common.js
@@ -35,37 +35,42 @@ $(".Accordion dt").on("click", function() {
 });
 
 //カルーセル
-$(function(){
-    var slideWidth = $('.Carousel-Wrapper li').outerWidth();
-    var slideNum = $('.Carousel-Wrapper li').length;
-    var slideSetWidth = slideWidth * slideNum;
-    $('.Carousel-Wrapper').css('width', slideSetWidth);
 
-    var slideCurrent = 0;// 現在地を示す変数
+var slideWidth = $('.Carousel-Wrapper li').outerWidth();  // .slideの幅を取得して代入
+var slideNum = $('.Carousel-Wrapper li').length;  // .slideの数を取得して代入
+var slideSetWidth = slideWidth * slideNum;  // .slideの幅×数で求めた値を代入
+$('.Carousel-Wrapper').css('width', slideSetWidth); // .slideSetのスタイルシートにwidth: slideSetWidthを指定
 
-    var sliding = function(){
-      // slideCurrentが0以下だったら
-      if( slideCurrent < 0 ){
-      slideCurrent = slideNum - 1;
-   // slideCurrentがslideNumを超えたら
-    }else if( slideCurrent > slideNum - 1 ){  // slideCUrrent >= slideNumでも可
-      slideCurrent = 0;
-    }
-    $('.Carousel-Wrapper').stop().animate({
-      left: slideCurrent * -slideWidth
-    });
-    }
-    // 前へボタンが押されたとき
-    $('.Carousel-prev').click(function(){
-      slideCurrent--;
-      sliding();
-    });
+var slideCurrent = 0; // 現在地を示す変数
 
-    // 次へボタンが押されたとき
-    $('.Carousel-next').click(function(){
-      slideCurrent++;
-      sliding();
-    });
+// アニメーションを実行する独自関数
+var sliding = function(){
+  // slideCurrentが0以下だったら
+  if( slideCurrent < 0 ){
+    slideCurrent = slideNum - 1;
 
+  // slideCurrentがslideNumを超えたら
+  }else if( slideCurrent > slideNum - 1 ){  // slideCUrrent >= slideNumでも可
+    slideCurrent = 0;
+
+  }
+
+  $('.Carousel-Wrapper').stop().animate({
+    left: slideCurrent * -slideWidth
+  });
+}
+
+// 前へボタンが押されたとき
+$('.Carousel-Button-Prev').click(function(){
+  slideCurrent--;
+  sliding();
 });
+
+// 次へボタンが押されたとき
+$('.Carousel-Button-Next').click(function(){
+  slideCurrent++;
+  sliding();
+});
+
+
 
